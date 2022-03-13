ecefc52e32910c564456bc6b0f531041b19181b0
AtienoObwanda <atienoobwanda@gmail.com>
2022-03-14 02:14:42 +0300

    created hover effect for the Portfolio section, using jQuery | Came up with 2 methods, opted to use the second method, the first method has animation

--- js/index.js
+++ js/index.js
@@ -33,3 +33,104 @@ $(document).ready(function(){
     $(".prodImg").slideDown('1500');
   });
 });
+
+// Overlay
+$(document).ready(function() {
+  $('#p1').bind('mouseover', function() {
+      $(this).fadeTo('fast', '.4');
+      $('.overlayText1').show();
+  }).bind('mouseout', function() {
+      $(this).fadeTo('fast', '1');
+      $('.overlayText1').hide();
+      
+  });
+});
+
+$(document).ready(function() {
+  $('#p2').bind('mouseover', function() {
+      $(this).fadeTo('fast', '.4');
+      $('.overlayText2').show();
+  }).bind('mouseout', function() {
+      $(this).fadeTo('fast', '1');
+      $('.overlayText2').hide();
+      
+  });
+});
+
+$(document).ready(function() {
+  $('#p3').bind('mouseover', function() {
+      $(this).fadeTo('fast', '.4');
+      $('.overlayText3').show();
+  }).bind('mouseout', function() {
+      $(this).fadeTo('fast', '1');
+      $('.overlayText3').hide();
+      
+  });
+});
+
+$(document).ready(function() {
+  $('#p4').bind('mouseover', function() {
+      $(this).fadeTo('fast', '.4');
+      $('.overlayText4').show();
+  }).bind('mouseout', function() {
+      $(this).fadeTo('fast', '1');
+      $('.overlayText4').hide();
+      
+  });
+});
+
+$(document).ready(function() {
+  $('#p5').bind('mouseover', function() {
+      $(this).fadeTo('fast', '.4');
+      $('.overlayText5').show();
+  }).bind('mouseout', function() {
+      $(this).fadeTo('fast', '1');
+      $('.overlayText5').hide();
+      
+  });
+});
+
+$(document).ready(function() {
+  $('#p7').bind('mouseover', function() {
+      $(this).fadeTo('fast', '.4');
+      $('.overlayText6').show();
+  }).bind('mouseout', function() {
+      $(this).fadeTo('fast', '1');
+      $('.overlayText6').hide();
+      
+  });
+});
+
+$(document).ready(function() {
+  $('#p8').bind('mouseover', function() {
+      $(this).fadeTo('fast', '.4');
+      $('.overlayText8').show();
+  }).bind('mouseout', function() {
+      $(this).fadeTo('fast', '1');
+      $('.overlayText8').hide();
+      
+  });
+});
+
+
+/*
+WORKS WELL, EXCEPT FOR THE ANIMATION OPTED TO GO WITH THE ALTERNATIVE
+$(document).ready(function () {
+  $('#p1').hover(function () {
+      $(this).stop().animate({
+          opacity: .4
+      }, 200);
+     //text
+     $(".overlayText1").show('1500');
+  }, function () {
+      $(this).stop().animate({
+          opacity: 1
+      }, 500);
+      //text
+      $(".overlayText1").hide('1000');
+  });
+});
+*/
+
+
+
