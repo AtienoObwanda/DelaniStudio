464b3321f845eb101412ca54fd2a83a2cfff3332
AtienoObwanda <atienoobwanda@gmail.com>
2022-03-13 04:19:39 +0300

    FInished adding the toggle functionality for the WHAT-WE-DO section. | everything workig perfectly. Yet to style the section

--- js/index.js
+++ js/index.js
@@ -1,37 +1,15 @@
-/*
-  $(document).ready(function() {
-    $(".devImg").click(function() {
-      $("#devImg").hide();
-      $("#dev").show();
-      
-    });
-    $(".dev").click(function() {
-      $("#dev").hide();
-      $("#devImg").show();
-    });
-    
-});
- */
-/*
-$(document).ready(function() {
-  $(".clickable").click(function() {
-    $("#walrus-showing").toggle();
-    $("#walrus-hidden").toggle();
-  });
-}); */
-
-
-/*
+//TOGGLE DESIGN
 $(document).ready(function(){
-  $(".devImg").click(function(){
-    $("#devImg").slideDown('1500').hide('1000');
-    $("#dev").show('1500');
+  $(".designImg").click(function(){
+    $(".designImg").hide('1000');
+    $("#design").show('1500');
   });
-  $("#dev").click(function(){
-    $("#dev").slideUp('1500');
-    $("#devImg").slideDown('1500');
+  $("#design").click(function(){
+    $("#design").slideUp('1500');
+    $(".designImg").slideDown('1500');
   });
-}); */
+});
+//TOGGLE DEVELOPMENT
 $(document).ready(function(){
   $(".devImg").click(function(){
     $(".devImg").hide('1000');
@@ -43,3 +21,15 @@ $(document).ready(function(){
   });
 });
 //hide("slide", { direction: "right" }, 1000);
+
+//TOGGLE PRODUCT MGT
+$(document).ready(function(){
+  $(".prodImg").click(function(){
+    $(".prodImg").hide('1000');
+    $("#product").show('1500');
+  });
+  $("#product").click(function(){
+    $("#product").slideUp('1500');
+    $(".prodImg").slideDown('1500');
+  });
+});
